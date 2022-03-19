6bce2c782af54fc2beca757b573959e91efcb024
comee-system <system@comee.jp>
2022-03-20 01:17:15 +0900

    協賛

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,9 +1,12 @@
 @section('header')
 
 <nav class="navbar navbar-expand navbar-dark bg-dark p-1">
-    <a class="navbar-brand" href="{{$open_url}}" target=_blank>{!! $title !!}</a>
     @if (!empty($open_url))
+    <a class="navbar-brand" href="{{$open_url}}" target=_blank>{!! $title !!}</a>
     <a href="{{$open_url}}" class="text-white" target=_blank>{{ $open_url }}</a>
+    @else
+    <h1 class="text-white">{!! $title !!}</h1>
+
     @endif
     <ul class="navbar-nav px-3 signout">
         <li class="nav-item text-nowrap">
